4a9879197677c6936ad8a4d5010c3141b0935877
Varsha <spvarsha6@gmail.com>
2025-09-16 02:12:26 +0530

    feat(course):[Day5] update CourseService

--- src/main/java/com/atlas/service/CourseService.java
+++ src/main/java/com/atlas/service/CourseService.java
@@ -5,10 +5,15 @@ import com.atlas.model.Course;
 
 import java.util.List;
 
+/**
+ * Small wrapper over CourseDao interface.
+ */
 public class CourseService {
-    private final CourseDao dao = new CourseDao();
+    private final CourseDao dao;
+
+    // constructor injection only
+    public CourseService(CourseDao dao) { this.dao = dao; }
 
     public Course get(String id) { return dao.getById(id); }
     public List<Course> list() { return dao.listAll(); }
 }
-
