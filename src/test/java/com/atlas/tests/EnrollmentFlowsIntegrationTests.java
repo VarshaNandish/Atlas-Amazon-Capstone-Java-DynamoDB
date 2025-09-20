cfe8b75f3c9b7412346b872af4ef647380f183e6
Varsha <spvarsha6@gmail.com>
2025-09-20 13:54:14 +0530

    test(integration):[Day9] update JUnit integration test class name

--- src/test/java/com/atlas/tests/EnrollmentFlowsIntegrationTests.java
+++ src/test/java/com/atlas/tests/EnrollmentFlowsIntegrationTests.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class EnrollmentFlowsIT {
+public class EnrollmentFlowsIntegrationTests {
 
     private DynamoDbClient client;
     private DynamoStudentDao studentDao;
